56934eca7be9c0a8955f6269bd6f0ed2ed70bcab
Taylor-Hallman <105246995+Taylor-Hallman@users.noreply.github.com>
2026-03-29 15:52:35 -0500

    fix list widget still being selectable with mouse

--- src/widgets/nouserselectlistwidget.h
+++ src/widgets/nouserselectlistwidget.h
@@ -10,18 +10,11 @@ class NoUserSelectListWidget : public QListWidget
 public:
     using QListWidget::QListWidget;
 protected:
-    void mousePressEvent(QMouseEvent* event) override {
-        event->ignore();
-    }
-    void mouseReleaseEvent(QMouseEvent* event) override {
-        event->ignore();
-    }
-    void mouseDoubleClickEvent(QMouseEvent* event) override {
-        event->ignore();
-    }
-    void keyPressEvent(QKeyEvent* event) override {
-        event->ignore();
-    }
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void mouseDoubleClickEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void keyPressEvent(QKeyEvent* event) override;
 };
 
 #endif // NOUSERSELECTLISTWIDGET_H
